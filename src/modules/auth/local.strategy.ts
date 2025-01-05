47e59869a382c15474dcbc7eb1ac9acd7b121660
Ugo Anayo <uggoprince@gmail.com>
2025-01-05 18:57:03 +0100

    make cors accept all

--- src/modules/auth/local.strategy.ts
+++ src/modules/auth/local.strategy.ts
@@ -2,6 +2,7 @@ import { Strategy } from 'passport-local';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { NotFoundError } from 'src/exceptions/notfound.exception';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
@@ -15,7 +16,7 @@ export class LocalStrategy extends PassportStrategy(Strategy) {
   async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
-      throw new UnauthorizedException('Invalid email/password');
+      throw new NotFoundError('Invalid email/password');
     }
     return user;
   }
